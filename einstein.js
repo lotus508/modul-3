35caf32dd5fce4cc1f07f8a2d5f5cd38f105538c
Жас <zhasulan@bk.ru>
2022-03-23 11:10:59 +0600

    степень 2

--- einstein.js
+++ einstein.js
@@ -1,3 +1,6 @@
-const myWeight = 92;
-const speedLight = 3e8;
-console.log('Энергия ' + myWeight * speedLight * speedLight);
+'use strict';
+//Вторая задача
+    const myWeight = 92;
+    const speedLight = 3e8;
+    
+    console.log('Энергия E=mc2: ' + myWeight * Math.pow(speedLight, 2));
--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-    var button = document.querySelector('button');
-
-    button.onclick = function() {
-        const product = prompt('Наименование товара', " ");
-        const amount = prompt('Количество товара');
-        const category = prompt('Категория товара');
-        const price = prompt('Цена товара');
+'use strict';
+//Первая задача
+    const product = prompt('Наименование товара');
+    const amount = prompt('Количество товара');
+    const category = prompt('Категория товара');
+    const price = prompt('Цена товара');
         
-        console.log('amount: ', typeof +amount);
-        console.log('price: ', typeof +price);
-        console.log('На складе ' + amount + 
-        ' единиц товара ' + '\"' + product + '\"' + 
-        ' на сумму ' + amount * price + ' рублей');
-    }  
+    console.log('amount: ', typeof +amount);
+    console.log('price: ', typeof +price);
+    console.log('На складе ' + amount + ' единиц товара ' + '\"' + product + '\"' + ' на сумму ' + 
+    amount * price + ' рублей');
     
